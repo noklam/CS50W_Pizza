94a546259f8ab3ca786a84f3421ab30d3039a408
noklam <channoklam@yahoo.com.hk>
2018-08-05 16:26:00 +0800

    First unittest for index page

--- orders/tests.py
+++ orders/tests.py
@@ -1,14 +1,18 @@
+import os
+import pathlib
 from django.test import TestCase
 from selenium import webdriver
-import os
+from django.urls import resolve
+from orders.views import index
 
 def file_uri(filename):
     return pathlib.Path(os.path.abspath(filename)).as_uri()
 
 DRIVER_PATH = pathlib.Path(os.path.abspath('geckodriver.exe')).as_posix() # On window, it's a bit weird, hacking for now.
 
-
-PATH = os.path.abspath('geckodriver.exe')
     # Create your tests here.
-def test_index(self):
-    self.assertEqual(response.status_code, 200)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_index_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)    
