276c9630e10c60085711ccf3be7f7fc3150b86ec
noklam <channoklam@yahoo.com.hk>
2018-08-05 19:40:03 +0800

    002 unit test

--- orders/tests.py
+++ orders/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from selenium import webdriver
 from django.urls import resolve
 from orders.views import index
+from django.http import HttpRequest
 
 def file_uri(filename):
     return pathlib.Path(os.path.abspath(filename)).as_uri()
@@ -16,3 +17,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_index_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, index)    
+
+    def test_index_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
+        self.assertIn(b'<title>Pizza</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.http import HttpRequest
+home_page = None
 # Create your views here.
 def index(request):
     return render(request, "orders/index.html")
