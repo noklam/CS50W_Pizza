e8ac3b8d91f4fd7d5a72e1f6d44f770cb2736027
noklam <channoklam@yahoo.com.hk>
2018-08-05 16:06:12 +0800

    update functional test with comments

--- orders/functional_tests.py
+++ orders/functional_tests.py
@@ -5,8 +5,8 @@ import pathlib
 
 def file_uri(filename):
     return pathlib.Path(os.path.abspath(filename)).as_uri()
-
-DRIVER_PATH = pathlib.Path(os.path.abspath('geckodriver.exe')).as_posix() # On window, it's a bit weird, hacking for now.
+# On window, it's a bit weird, hacking the driver for now.
+DRIVER_PATH = pathlib.Path(os.path.abspath('geckodriver.exe')).as_posix() 
 
 class NewVistorTest(unittest.TestCase):
 
@@ -18,10 +18,19 @@ class NewVistorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_index_page_correct_title(self):
+    def test_can_add_items_to_shopping_cart_and_retreive_later(self):
+        # Nok heard about a nice pizza shop
+        # He checks their homepage
         self.browser.get('http://localhost:8000')
-        self.assertEqual('Pizza', self.browser.title)
 
+        #He found the title of the web shows 'Pizza'
+        self.assertEqual('Pizza', self.browser.title)
+        self.fail('Finish the test!')
+
+        # He saw a menus for pizza abd various types of items
+        # He try to add a small pizza with pepperoni into the shopping cart
+        # The web shows a total price of the pizza that he ordered
+        # He try to checkout
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
